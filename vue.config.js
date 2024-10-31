2f66ed2635b946998d7b64199df06b9cf5931cc8
vue-my-project <vitalay.efimov@gmail.com>
2024-10-31 20:05:31 +0500

    кавыряние

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = defineConfig({ 
+  publicPath: process.env.NODE_ENV === 'production' ? '/testttt/' : '/',
   transpileDependencies: true
 })
